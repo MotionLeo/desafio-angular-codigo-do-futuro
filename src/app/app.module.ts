afeca7fcfe683c4633f8c509bb695752f3c78b85
rwsolutions07 <101687916+Williamalmeida27@users.noreply.github.com>
2022-11-28 18:08:50 -0300

    Atualização do module routing
    Alteração do routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainRoutingModule } from './main/main-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MainRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
