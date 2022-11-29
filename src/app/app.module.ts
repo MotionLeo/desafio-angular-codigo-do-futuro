3f9a85c8b3f7551b92b3137e963c0e0de6ae98c0
rwsolutions07 <101687916+Williamalmeida27@users.noreply.github.com>
2022-11-28 22:10:03 -0300

    Atualizações
    Ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MainRoutingModule } from './main/main-routing.module';
+import { MainRoutingModule } from '../app/main/main-routing.module';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './pages/navegacao/header/header.component';
 import { ListaClienteComponent } from './pages/lista-cliente/lista-cliente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     ListaClienteComponent
   ],
   imports: [
--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaClienteComponent } from '../pages/lista-cliente/lista-cliente.component';
+import { LoginComponent } from '../pages/login/login.component';
 
 const routes: Routes = [
   {path: 'clientes', component: ListaClienteComponent},
+  {path: 'login', component: LoginComponent},
 ];
 
 @NgModule({
