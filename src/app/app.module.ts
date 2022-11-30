4b6aa69465796f9e39c257491d60084a2f9959cc
gabriel0w <gabrielo.dzn@gmail.com>
2022-11-30 18:31:40 -0300

    Componente de lista de produtos adicionado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ListaClienteComponent } from './pages/lista-cliente/lista-cliente.component';
 import { HeaderComponent } from './pages/navegacao/header/header.component';
+import { ProdutosComponent } from './pages/produtos/produtos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListaClienteComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProdutosComponent
   ],
   imports: [
     BrowserModule,
