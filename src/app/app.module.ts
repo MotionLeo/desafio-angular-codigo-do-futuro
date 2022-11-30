4f0b6b955b4c7837d86d0dcd2ce1282962998863
rwsolutions07 <101687916+Williamalmeida27@users.noreply.github.com>
2022-11-29 22:07:08 -0300

    Pagina-login
    Atualizações de pag login, header com infos de produtos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { MainRoutingModule } from '../app/main/main-routing.module';
 
 import { AppComponent } from './app.component';
 import { ListaClienteComponent } from './pages/lista-cliente/lista-cliente.component';
+import { LoginComponent } from './pages/login/login.component';
 import { HeaderComponent } from './pages/navegacao/header/header.component';
+import { LoginGuard } from './servicos/login.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListaClienteComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
-    MainRoutingModule
+    MainRoutingModule,
+    FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    LoginGuard,
+  ],
+  bootstrap: [AppComponent],
+  
 })
 export class AppModule { }
