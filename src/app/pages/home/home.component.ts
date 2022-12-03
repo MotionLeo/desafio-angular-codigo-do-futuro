07c8363d2db104a450da40c159e4944196d7a102
MotionLeo <leodaniel.1998@hotmail.com>
2022-12-03 10:51:48 -0300

    iniciado página home e fluxo de caixa nele

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Pedido } from 'src/app/models/pedido';
+import { PedidoServico } from 'src/app/servicos/pedidoServico';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+  ) { }
+
+  public pedidosServico: PedidoServico = {} as PedidoServico;
+  public pedidos: Pedido[] | undefined = [];
 
   ngOnInit(): void {
+    this.pedidosServico = new PedidoServico(this.http);
+    this.listaDePedidos();
   }
+  
 
+  private async listaDePedidos(){
+    this.pedidos = await this.pedidosServico.lista();
+  }
+  number (a : Number){
+    return Number(a)
+  }
 }
